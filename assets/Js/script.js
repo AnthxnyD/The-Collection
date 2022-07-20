6ee76092a391ba6455087cf7d48f71f0b9461c54
Anthony <anthony.denis.becode@gmail.com>
2022-07-20 16:28:17 +0200

    Ajout fichier

---
+++ assets/Js/script.js
@@ -0,0 +1,115 @@
+const collection = [
+    {
+      name: "All Eyez on Me",
+      artiste: "Tupac Shakur",
+      releaseDate: "13 février 1996",
+      picture:"/assets/Pictures/TupacShakur.jpg",
+      genre: ["Hip-hop", "Gangsta rap"],
+      
+    },
+    {
+        name: "UMLA",
+        artiste: "Alpha Wann",
+        releaseDate: "21 septembre 2018",
+        picture:"/assets/Pictures/AlphaOne.jpg",
+        genre: ["Hip-hop/Rap", "French Rap"],
+        
+      },
+      {
+        name: "JVLIVS II",
+        artiste: "SCH",
+        releaseDate: "19 mars 2021",
+        picture:"/assets/Pictures/JVLIVSII.jpg",
+        genre: ["Hip-hop/Rap", "French Rap"],
+       
+      },
+      {
+        name: "LMF",
+        artiste: "Freeze Corleone",
+        releaseDate: "11 septembre 2020",
+        picture:"/assets/Pictures/FreezeCorleone.jpg",
+        genre: ["Hip-hop/Rap", "French Rap"],
+        
+      },
+      {
+        name: "Members Only, Vol. 4",
+        artiste: "Members Only",
+        releaseDate: "23 janvier 2019",
+        picture:"/assets/Pictures/MembersOnlyVol4.jpg",
+        genre: ["Hip-hop"],
+        
+      },
+      {
+        name: "Meet the Woo 2",
+        artiste: "Pop Smoke",
+        releaseDate: "7 février 2020",
+        picture:"/assets/Pictures/PopSmoke.jpg",
+        genre: ["Hip-hop"],
+        
+      },
+      {
+        name: "CHOMP 2",
+        artiste: "Russ",
+        releaseDate: "8 décembre 2021",
+        picture:"/assets/Pictures/CHOMP2.jpg",
+        genre: ["Hip-hop", "Variété"],
+        
+      },
+      {
+        name: "Look at Me: The Album",
+        artiste: "XXXTentacion",
+        releaseDate: "10 juin 2022",
+        picture:"/assets/Pictures/LookAtMe.jpg",
+        genre: ["Hip-hop","RAP US"],
+        
+      },
+      {
+        name: "V",
+        artiste: "Vald",
+        releaseDate: "4 février 2022",
+        picture:"/assets/Pictures/PopSmoke.jpg",
+        genre: ["Hip-hop/Rap", "French Rap"],
+        
+      },
+      {
+        name: "Poésie d'une pulsion",
+        artiste: "B.B Jacques",
+        releaseDate: "2 février 2022",
+        picture:"/assets/Pictures/PoesiePulsion.jpg",
+        genre: ["Hip-hop/Rap", "French Rap"],
+        
+      },
+  ];
+
+  for(let elements of collection){
+
+    
+  }
+
+    /*
+    for(let i = 0; i< collection.length; i++){
+
+    let Card = `  
+    <div class="Card">           
+    <div class="Card-image">  
+
+    </div>
+    <div class="Card-Gender">
+        <span class = "genre"> ${collection[i].genre[0]} </span>
+        
+        
+    </div>
+    <div class="Card-Title">
+        <h3>Album :</h3>
+    </div>
+    <div class="Card-Artiste">
+        <h3>Artiste :</h3>
+    </div>
+    <div class="Card-Description">
+        <p>TEST BLABLABLA LOREM IPSUM</p>
+    </div>
+    </div>`;
+    
+    document.getElementById('main').insertAdjacentHTML("beforeend", Card)
+  }
+  */
